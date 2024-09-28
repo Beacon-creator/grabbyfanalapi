f114163df16f1fca2f5503a3ec427a3be7331593
Beacon-creator <adebiyibukola157@gmail.com>
2024-09-28 10:49:29 +0100

    major commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+'use strict';
+import { Router } from 'express';
+var router = Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+export default router;
